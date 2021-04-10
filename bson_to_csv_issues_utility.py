1c150fe7436222890053eee614c1e0dece7a6386
KasparasKralikas <kasparaskralikas@gmail.com>
2021-04-10 18:49:15 +0300

    Optimizing performance for bson to csv

--- bson_to_csv_issues_utility.py
+++ bson_to_csv_issues_utility.py
@@ -11,15 +11,20 @@ def main():
     issues = None
     f = open(input_file, 'rb')
     issues = np.asarray(bson.decode_all(f.read()))
-    df = pd.DataFrame(columns=['bug_id', 'dup_id', 'master_id', 'short_desc', 'description'])
+    #df = pd.DataFrame(columns=['bug_id', 'dup_id', 'master_id', 'short_desc', 'description'])
+    rows_list = []
+    count = 0
     for issue in issues:
-        df = df.append({
+        count += 1
+        print(count)
+        rows_list.append({
             'bug_id': int(issue['bug_id']),
             'dup_id': None if not issue['dup_id'] else int(issue['dup_id']),
             'master_id': int(issue['bug_id']) if not issue['dup_id'] else int(issue['dup_id']),
             'short_desc': str(issue['short_desc']),
             'description': str(issue['description'])
-        }, ignore_index=True)
+        })
+    df = pd.DataFrame(rows_list)
     df = df.sort_values(by='bug_id')
     df.to_csv(output_file, index=False)
     return
