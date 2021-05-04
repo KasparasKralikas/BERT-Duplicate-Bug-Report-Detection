a3390c15af855bbfa303845bca102f3bbb78d758
KasparasKralikas <kasparaskralikas@gmail.com>
2021-05-04 18:25:59 +0300

    Implemented top-k similar issues retrieval by description for the web app

--- dbrd_web_api.py
+++ dbrd_web_api.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
+from flask_cors import CORS
 import os.path as path
 import pandas as pd
 import numpy as np
 import pickle
 import json
+import time
 
 MODEL_PATH = 'model/paraphrase-distilroberta-dbrd-ooall-40000'
 
@@ -21,12 +23,17 @@ embeddings = None
 PORT = 5000
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/get_duplicates', methods=['POST'])
+@app.route('/get_duplicates/', methods=['POST'])
 def get_duplicates():
     body = request.get_json(silent=True)
     description = body['description']
-    return 'Hello World of Bugs'
+    top_k = body['top_k']
+    similar_issues = retrieve_top_k_similar_issues(model, issues, embeddings, description, top_k)
+    return {
+        'similar_issues': similar_issues
+    }
 
 def load_model():
     print('Loading model...')
@@ -57,6 +64,25 @@ def generate_embeddings(model, issues):
     pickle.dump(embeddings, open(EMBEDDINGS_OUTPUT, 'wb'))
     print('Embeddings saved')
     return embeddings
+
+def retrieve_top_k_similar_issues(model, issues, embeddings, description, top_k):
+    print('Retrieving top-{} similar issues...'.format(top_k))
+    start_time = time.time()
+    description_embedding = model.encode(description, convert_to_tensor=True)
+    results = util.semantic_search(description_embedding, embeddings, top_k=top_k)[0]
+    similar_issues = []
+    for result in results:
+        result_issue = issues.iloc[result['corpus_id']]
+        issue = {
+            'id': int(result_issue['bug_id']),
+            'description': str(result_issue['full_description']),
+            'similarity': float(result['score'])
+        }
+        similar_issues.append(issue)
+    end_time = time.time()
+    duration_in_secs = end_time - start_time
+    print('Retrieved top-{} similar issues in {} seconds'.format(top_k, round(duration_in_secs, 5)))
+    return similar_issues
 
 if __name__ == '__main__':
     model = load_model()
